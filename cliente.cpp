5f6be34ba0f48265aaf8b15c0c3cd49dbc64f7fe
Diego Pacheco <dipacmon@live.com>
2020-04-07 12:06:06 -0500

    Version Actualizada

--- cliente.cpp
+++ cliente.cpp
@@ -2,34 +2,29 @@
 #include<cstring>
 #include "Solicitud.h"
 #include <time.h>
+//
+
+
 
 using namespace std;
+int main(int argc, char* argv[]) {
+  srand(time(NULL));
+  int contador = 0;
+  int n = atoi(argv[2]);
+  while (contador<10) {
+	int num= 1 + rand() % 9;;
+	int numRespuesta;
+	Solicitud solicitud;
+	//strcpy(numRespuesta,solicitud.doOperation(argv[1],7200,1,(char*)&num));
+	memcpy(&numRespuesta,solicitud.doOperation(argv[1],7200,1,(char*)&num),2*sizeof(int));
 
-int main(int argc, char *argv[]) {
-    int numRespuesta[2];
-    int contador = 0;
-    int total = 0;
+	//solicitud.prueba(10);
+	//memcpy(&numRespuesta,solicitud.doOperation(argv[1],6666,1,(char*)&num),10);
+	//cout<<numRespuesta<<endl;
+  cout << " Transaccion: "<<contador<<" Cantidad: "<<num<<" Total: "<<numRespuesta<<endl;
 
-
-    int n = atoi(argv[2]);
-    Solicitud solicitud;
-
-        srand(time(NULL));
-
-    while (contador < n) {
-        int transacciones = 1 + rand() % 9;
-        cout << transacciones<<endl;
-        memcpy(&numRespuesta, solicitud.doOperation(argv[1], 7200, 1, (char *) &transacciones), 2*sizeof(int));
-/*
-        printf("Deposito de %d total %d " ,numRespuesta[0],numRespuesta[1]);
-*/
-
-        total = total + transacciones;
-        cout << "Deposito de "<< numRespuesta[0] << " total de " <<total<<endl;
-        contador++;
-
-    }
-
-
-    return 0;
+	//printf("Transaccion:%d\nCantidad:%d\nTotal:%d", contador, num,numRespuesta);
+  contador++;
+  }
+	return 0;
 }
--- servidor.cpp
+++ servidor.cpp
@@ -6,6 +6,32 @@ using namespace std;
 
 int main() {
     Respuesta resp = Respuesta(7200);
+    struct mensaje* msg;
+    int total = 0;
+    int contador=0;
+
+      cout<< "Esperando Respuesta:"<<endl;
+    while(1){
+      int numeros;
+      msg=resp.getRequest();
+      //sscanf(msg->arguments, "%d %d", &a, &b);
+        //memcpy((char*)a,msg->arguments, 2*sizeof(int));
+
+          //memcpy((char*)b,msg->arguments, 2*sizeof(int));
+        //strcpy(&numeros,&msg->arguments);
+        memcpy(&numeros,&msg->arguments, 2*sizeof(int));
+      total = total+numeros;
+
+      cout << " Cantidad: "<<numeros<<" Total: "<<total<<endl;
+      cout<<total<<endl;
+
+        resp.sendReply(&total);
+    }
+}
+
+/*
+int main() {
+    Respuesta resp = Respuesta(7200);
     int nbd = 0;
     int total = 0;
     int respuesta[2];
@@ -13,18 +39,20 @@ int main() {
 
 
     while (1) {
+
+      int arreglo[2];
         cout << "Esperando Respuesta:" << endl;
         msg = resp.getRequest();
-        memcpy(&nbd, &msg->arguments, sizeof(int));
-        total = total + nbd;
-        cout << "Se recibio una transferencia por: " << nbd << " El total de la cuenta es: " << total << endl;
-        respuesta[0] = nbd;
+        memcpy(&arreglo, &msg->arguments, sizeof(int));
+        total = total + arreglo[1];
+        cout << "Transaccion numero: "<<arreglo[0] <<" Se recibio una transferencia por: " << arreglo[1] << " El total de la cuenta es: " << total << endl;
+        respuesta[0] = arreglo[1];
         respuesta[1] = total;
-        resp.sendReply((int *) respuesta);
+        resp.sendReply((int *) &respuesta);
 
 
     }
 
 }
-
+*/
 //(char *) num
