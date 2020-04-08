40a23c96d76d663aa3990be565d8a54006e81fc3
Diego Pacheco <dipacmon@live.com>
2020-04-08 16:49:12 -0500

    Final

--- cliente.cpp
+++ cliente.cpp
@@ -11,8 +11,11 @@ int main(int argc, char* argv[]) {
   srand(time(NULL));
   int contador = 0;
   int n = atoi(argv[2]);
+  int transaccion;
   while (contador<10) {
 	int num= 1 + rand() % 9;;
+
+  transaccion=num;
 	int numRespuesta;
 	Solicitud solicitud;
 	//strcpy(numRespuesta,solicitud.doOperation(argv[1],7200,1,(char*)&num));
@@ -21,9 +24,9 @@ int main(int argc, char* argv[]) {
 	//solicitud.prueba(10);
 	//memcpy(&numRespuesta,solicitud.doOperation(argv[1],6666,1,(char*)&num),10);
 	//cout<<numRespuesta<<endl;
-  cout << " Transaccion: "<<contador<<" Cantidad: "<<num<<" Total: "<<numRespuesta<<endl;
+  //cout << " Transaccion: "<<contador<<" Cantidad: "<<num<<" Total: "<<numRespuesta<<endl;
 
-	//printf("Transaccion:%d\nCantidad:%d\nTotal:%d", contador, num,numRespuesta);
+	printf("Transaccion:%d\nCantidad:%d\nTotal:%d\n", contador, transaccion,numRespuesta);
   contador++;
   }
 	return 0;
